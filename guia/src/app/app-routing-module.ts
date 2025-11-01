1cfb62f6741831db392725be673698a2c3720d61
payan3521 <pablobedoya3521@gmail.com>
2025-11-01 16:17:59 -0500

    separacion de responsabilidades de las rutas

--- guia/src/app/app-routing-module.ts
+++ guia/src/app/app-routing-module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DesignShowcase } from './atomic/pages/design-showcase/design-showcase';
 import { LoginPage } from './atomic/pages/login-page/login-page';
-
-const routes: Routes = [
-  { path: 'design-showcase', component: DesignShowcase },
-  { path: 'login', component: LoginPage },
-  { path: '**', redirectTo: 'login', pathMatch: 'full' }
-];
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
